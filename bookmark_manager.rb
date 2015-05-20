8f7321ba37363b0d45473f2e412cc3240f5cd1ea
smarbaf <smarbaf@hotmail.com>
2015-05-20 12:30:36 +0100

    Filtering by tag

--- bookmark_manager.rb
+++ bookmark_manager.rb
@@ -17,24 +17,30 @@ class BookmarkManager < Sinatra::Base
     url = params['url']
     title = params['title']
     tags = params['tags'].split(' ').map do |tag|
-#   # this will either find this tag or create
-#   # it if it doesn't exist already
-    Tag.first_or_create(text: tag)
+    #   # this will either find this tag or create
+    #   # it if it doesn't exist already
+      Tag.first_or_create(text: tag)
+    end
+      Link.create(url: url, title: title, tags: tags)
+      redirect to('/')
   end
-    Link.create(url: url, title: title, tags: tags)
-    redirect to('/')
+
+  get '/tags/:text' do
+    tag = Tag.first(text: params[:text])
+    @links = tag ? tag.links : []
+    erb :index
   end
 
-env = ENV['RACK_ENV'] || 'development'
+  env = ENV['RACK_ENV'] || 'development'
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+  DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
-require './lib/link'
+  require './lib/link'
 
-DataMapper.finalize
+  DataMapper.finalize
 
-DataMapper.auto_upgrade!
+  DataMapper.auto_upgrade!
 
-# start the server if ruby file executed directly
+  # start the server if ruby file executed directly
   run! if app_file == $0
 end
